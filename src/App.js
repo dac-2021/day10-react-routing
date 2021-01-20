4f71b313c739f9f6afb25ea28175a6517fd77435
koderoom <koderoom.dev@gmail.com>
2021-01-20 17:07:04 +0530

    simplest routing.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+/**
+ * http://localhost:3000/
+ * http://localhost:3000/page1
+ * http://localhost:3000/page2
+ * http://localhost:3000/page3
+ */
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Route exact path="/">
+        <div>Default Page</div>
+      </Route>
+
+      <Route path="/page1">
+        <div>Page 1</div>
+      </Route>
+
+      <Route path="/page2">
+        <div>Page 2</div>
+      </Route>
+
+      <Route path="/page3">
+        <div>Page 3</div>
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
